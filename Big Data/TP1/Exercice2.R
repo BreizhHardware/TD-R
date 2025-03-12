d2a5a4faa80071f7d9079d556ee23b01e385b8af
Félix MARQUET <felix.marquet@isen-ouest.yncrea.fr>
2025-03-12 10:48:18 +0100

    fix: correct histogram title formatting and clean up code structure

--- Big Data/TP1/Exercice2.R
+++ Big Data/TP1/Exercice2.R
@@ -12,10 +12,10 @@ for (j in(1:50)){
     # puis normalisation: (moyenne-mu)/sigma
     ms[i]=(mean(rbinom(k,1,p))-mu)/sig }
   # Tracé de l'histogramme des moyennes normalisées
-  hist(ms, breaks=41, xlab="x-variable", xlim=c(-4,4), prob=TRUE, main=sprintf("normal curve over
-histogram, n = %d",k))
+  hist(ms, breaks=41, xlab="x-variable", xlim=c(-4,4), prob=TRUE, main=sprintf("normal curve over histogram, n = %d",k))
   # Superposition de la courbe de densité normale standard N(0,1)
-  curve(dnorm(x), col="darkblue", lwd=2, add=TRUE, yaxt="n")}
+  curve(dnorm(x), col="darkblue", lwd=2, add=TRUE, yaxt="n")
+}
 
 
 # Définition de la séquence pour le tracé des courbes
@@ -25,25 +25,19 @@ x = seq(-5, 5, 0.025)
 for (j in (1:50)) {
   # k = taille d'échantillon (j²), donc de 1 à 2500
   k = j*j
-
   for (i in (1:10000)) {
     # Génération de k variables Cauchy et calcul de leur moyenne
     ms[i] = mean(rcauchy(k))
   }
-
   # Pour éviter que les valeurs extrêmes ne déforment l'histogramme
   valid_range = ms > -5 & ms < 5
-
   # Tracé de l'histogramme des moyennes
   hist(ms[valid_range], breaks=41, xlab="x-variable", xlim=c(-5, 5),
        prob=TRUE, main=sprintf("Distribution des moyennes de Cauchy, n = %d", k))
-
   # Superposition de la courbe de densité normale standard N(0,1)
   curve(dnorm(x), col="darkblue", lwd=2, add=TRUE, yaxt="n")
-
   # Superposition de la courbe de densité Cauchy standard
   curve(dcauchy(x), col="darkred", lwd=2, add=TRUE, yaxt="n")
-
   # Légende
   legend("topright", legend=c("Normale N(0,1)", "Cauchy"),
          col=c("darkblue", "darkred"), lwd=2)
